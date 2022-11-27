7f923b20097d8a7885a82829e431a86730c6bc3f
Pro100-Adm <kirow.alesha@yandex.ru>
2022-11-27 19:27:21 +0300

    Kirov A.D. seminar 27.11.2022

--- homework/task_2/Program.cs
+++ homework/task_2/Program.cs
@@ -23,6 +23,14 @@ else{
 }
 
 Point cross_point(double k1, double b1, double k2, double b2){
+if(k1 == k2 && b1 == b2){
+    Console.WriteLine("Прямые совпадают!");
+    return cross_point(AskUser("Введите k1: "), AskUser("Введите b1: "), AskUser("Введите k2: "), AskUser("Введите b2: "));
+}
+if(k1 == k2){
+    Console.WriteLine("Прямые параллельны!");
+    return cross_point(AskUser("Введите k1: "), AskUser("Введите b1: "), AskUser("Введите k2: "), AskUser("Введите b2: "));
+}
 double x = (b2-b1)/(k1-k2);
 double y = ((k1*(b2-b1))/(k1-k2))+b1;
 Point cross = new Point(x, y);
